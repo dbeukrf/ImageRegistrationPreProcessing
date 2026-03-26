b3f829516c79d0585221060eb7c6972994ba88a9
diegobeu <dbeu0001@student.monash.edu>
2026-03-26 12:31:39 +1100

    added python scripts for kaggle download

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from imageregistrationpreprocessing!")
+import kagglehub
 
+
+def main():
+
+    path = kagglehub.dataset_download("awsaf49/brats20-dataset-training-validation")
+    print("Path to dataset files:", path)
 
 if __name__ == "__main__":
     main()
